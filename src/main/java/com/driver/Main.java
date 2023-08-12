91626b66fa082cbc338c9893a72805b8d5998d68
somalingck <136596008+somalingck@users.noreply.github.com>
2023-08-12 09:49:46 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        int result1 = p.product(10, 5);
+        int result2 = p.product(10, 5, 2);
+        double result3 = p.product(5.02, 3.01);
+
+        System.out.println("Result 1: " + result1);
+        System.out.println("Result 2: " + result2);
+        System.out.println("Result 3: " + result3);
+    }
 }
